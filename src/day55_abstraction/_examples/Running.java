1de77914121faa6173c23bfc0904a6b682bccea5
Dilnoza <dilnoz8605@icloud.com>
2021-07-13 20:03:21 -0500

    abstract class practice

---
+++ src/day55_abstraction/_examples/Running.java
@@ -0,0 +1,14 @@
+package day55_abstraction._examples;
+
+public class Running extends Exercise{
+
+    @Override
+    public void perform() {
+
+    }
+
+    @Override
+    public int getCaloriesCount(int minutes) {
+        return 0;
+    }
+}
